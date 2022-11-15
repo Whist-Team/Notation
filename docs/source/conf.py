31d0bf2a0c7ff70c44d283dbf04e88b70dbc2d6b
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2022-11-15 20:48:26 +0100

    ENABLE: autosection
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ extensions = [
 ]
 
 autoapi_dirs = ['../../notation']
-
+autosectionlabel_prefix_document = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
