a6be8865cd454329609880e31b06158f5dc97a72
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2023-07-31 23:08:14 +0200

    ADD: more tests
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,8 +1,9 @@
 import pytest
-from hypothesis import given, example
-from hypothesis.strategies import text
+import tomlkit
+from hypothesis import given, assume
+from hypothesis.strategies import text, characters
 
-from notation.card import Card
+from notation.card import Card, FrenchCard
 
 
 @given(text(min_size=1), text(min_size=1))
@@ -22,3 +23,41 @@ def test_init_empty_rank(suit):
     with pytest.raises(ValueError):
         _ = Card(suit, '')
 
+
+@given(text(min_size=1, alphabet=characters(whitelist_categories=('L', 'N', 'S'),
+                                            blacklist_characters=('"', "'"))),
+       text(min_size=1, alphabet=characters(whitelist_categories=('L', 'N', 'S'),
+                                            blacklist_characters=('"', "'"))))
+def test_dumps(suit, rank):
+    card = Card(suit, rank)
+    toml_card = card.dumps()
+    expected_toml = f'''\
+suit = "{suit}"
+rank = "{rank}"
+'''
+    assume(tomlkit.loads(expected_toml))
+    assert toml_card == expected_toml
+
+
+@given(text(min_size=1, alphabet=characters(whitelist_categories=('L', 'N', 'S'),
+                                            blacklist_characters=('"', "'", '♦', '♣', '♥', '♠'))),
+       text(min_size=1, alphabet=characters(whitelist_categories=('L', 'N', 'S'),
+                                            blacklist_characters=(
+                                            '"', "'", 'A', '2', '3', '4', '5', '6', '7',
+                                            '8', '9', 'J', 'Q', 'K'))))
+def test_init_french_invalid(suit, rank):
+    with pytest.raises(ValueError):
+        _ = FrenchCard(suit, rank)
+
+
+@pytest.mark.parametrize("suit", ['♦', '♣', '♥', '♠'])
+@pytest.mark.parametrize("rank", ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'])
+def test_french_dumps(suit, rank):
+    card = FrenchCard(suit, rank)
+    toml_card = card.dumps()
+    expected_toml = f'''\
+suit = "{suit}"
+rank = "{rank}"
+'''
+    assume(tomlkit.loads(expected_toml))
+    assert toml_card == expected_toml
