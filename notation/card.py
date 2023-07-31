ba5fc0ec7c8c3faf9e17d3ccd2041fbd528ea09f
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2023-07-31 16:46:27 +0000

    ADD: general validation

--- notation/card.py
+++ notation/card.py
@@ -3,8 +3,24 @@ import tomli_w
 
 class Card:
     def __init__(self, suit: str, rank: str):
-        self._suit: str = suit
-        self._rank: str = rank
+        self._suit: str = self._valid_suit(suit)
+        self._rank: str = self._valid_rank(rank)
+    
+    @staticmethod
+    def _valid_suit(suit: str)->str:
+        if not isinstance(suit, str):
+            raise ValueError('Suit must be string.')
+        if not suit:
+            raise ValueError('Suit cannot be empty.')
+        return suit
+
+    @staticmethod
+    def _valid_rank(rank: str) -> str:
+        if not isinstance(rank, str):
+            raise ValueError('Rank must be string.')
+        if not rank:
+            raise ValueError('Rank must not be empty.')
+        return rank
 
     def dumps(self) -> str:
         return tomli_w.dumps({'suit': self._suit, 'rank': self._rank})
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,5 +1,21 @@
-from unittest import TestCase
+import pytest
+from hypothesis import given, example
+from hypothesis.strategies import text
+
+from notation.card import Card
 
 
-class CardTestCase(TestCase):
-  pass
+@given(text(min_size=1), text(min_size=1))
+def test_dumps(suit, rank):
+  card = Card(suit, rank)
+  assert isinstance(card, Card)
+
+@given(text())
+def test_dumps_empty_suit(rank):
+  with pytest.raises(ValueError):
+    _ = Card('', rank)
+
+@given(text())
+def test_dumps_empty_rank(suit):
+  with pytest.raises(ValueError):
+    _ = Card(suit, '')
