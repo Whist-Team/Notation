e1ac6ab10cf9bd32529fd904363a399ae9f0b758
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2023-07-31 21:35:45 +0200

    REFACTOR: rename test
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- tests/test_card.py
+++ tests/test_card.py
@@ -6,18 +6,19 @@ from notation.card import Card
 
 
 @given(text(min_size=1), text(min_size=1))
-def test_dumps(suit, rank):
+def test_init(suit, rank):
     card = Card(suit, rank)
     assert isinstance(card, Card)
 
 
 @given(text())
-def test_dumps_empty_suit(rank):
+def test_init_empty_suit(rank):
     with pytest.raises(ValueError):
         _ = Card('', rank)
 
 
 @given(text())
-def test_dumps_empty_rank(suit):
+def test_init_empty_rank(suit):
     with pytest.raises(ValueError):
         _ = Card(suit, '')
+
