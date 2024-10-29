342cc33d2566e2701a70a90b7f09bac71ae7606f
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-10-29 21:53:07 +0100

    REFACTOR: constants

--- notation/card.py
+++ notation/card.py
@@ -1,12 +1,21 @@
 """Python implementation of a general card and specific versions."""
 import tomlkit
 
+FRENCH_SUITS = ('♦', '♣', '♥', '♠')
+
+FRENCH_RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+
 
 class Card:
-    """
-    General representation of a card.
-    """
-    def __init__(self, suit: str, rank: str):
+    """General representation of a card."""
+
+    def __init__(self, suit: str, rank: str) -> None:
+        """
+        Construct a card.
+
+        :param suit: The suit of the card.
+        :param rank: The rank of the card.
+        """
         self._suit: str = self._valid_suit(suit)
         self._rank: str = self._valid_rank(rank)
 
@@ -37,20 +46,21 @@ class Card:
         return self._rank
 
     def dumps(self) -> str:
-        """Returns the card as TOML string."""
+        """Return the card as TOML string."""
         return tomlkit.dumps({'suit': self._suit, 'rank': self._rank})
 
 
 class FrenchCard(Card):
     """Represents a French card."""
+
     @staticmethod
     def _valid_suit(suit: str) -> str:
-        if suit not in ('♦', '♣', '♥', '♠'):
+        if suit not in FRENCH_SUITS:
             raise ValueError('Not a correct french suit.')
         return suit
 
     @staticmethod
     def _valid_rank(rank: str) -> str:
-        if rank not in ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'):
+        if rank not in FRENCH_RANKS:
             raise ValueError('Not a correct french rank.')
         return rank
