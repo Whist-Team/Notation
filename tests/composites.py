1bbc37eddf2e120e372a0746204eb0c8abb7d99a
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-11-01 20:08:26 +0100

    ADD: builder player

--- tests/composites.py
+++ tests/composites.py
@@ -1,7 +1,7 @@
-from hypothesis import assume
-from hypothesis.strategies import composite, text, characters
+from hypothesis.strategies import composite, text, characters, integers
 
 from notation.card import Card
+from notation.player import Player
 
 
 @composite
@@ -13,3 +13,10 @@ def build_card(draw):
                           alphabet=characters(whitelist_categories=('L', 'N', 'S'),
                                               blacklist_characters=('"', "'"))))
     return Card(suit=suit, rank=rank)
+
+
+@composite
+def build_player(draw):
+    name = draw(text(alphabet=characters()))
+    team = draw(integers())
+    return Player(name, team)
