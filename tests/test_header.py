2d2c82bf86be8e3d9b5dfcafd95f5659f594cf26
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-11-02 10:49:29 +0100

    UPDATE: test edge cases

--- tests/test_header.py
+++ tests/test_header.py
@@ -3,16 +3,25 @@ from datetime import datetime
 import pytest
 import tomlkit
 from hypothesis import assume, example, given
-from hypothesis.strategies import datetimes, lists, integers
+from hypothesis.strategies import datetimes, lists, integers, composite
 
 from notation.header import Header
 from notation.player import Player
 from tests.composites import build_player
 
 
-@given(datetimes(), lists(build_player(), max_size=6), integers())
-@example(datetime.now(), [Player(name='John', team=1)], 1)
-def test_dumps(start_time: datetime, players, number_teams):
+@composite
+def build_header_dumps(draw):
+    timestamp = draw(datetimes())
+    players = draw(lists(build_player(), min_size=1))
+    teams = draw(integers(max_value=len(players)))
+    return (timestamp, players, teams)
+
+
+@given(build_header_dumps())
+@example((datetime.now(), [Player(name='John', team=1)], 1))
+def test_dumps(data):
+    start_time, players, number_teams = data
     header = Header(start_time=start_time, players=players, number_teams=number_teams)
     header_dump = header.dumps()
     player_string = ((f'[[players.seat_{index}]]\n'
@@ -20,7 +29,7 @@ def test_dumps(start_time: datetime, players, number_teams):
                       f'team = {player._team}') for index, player in enumerate(players))
     expected_toml = (f'[header]\ntimestamp = "{start_time.isoformat()}"\nnumber_of_teams = '
                      f'{number_teams}\n\n')
-    expected_toml += "\n".join(player_string) + '\n'
+    expected_toml += "\n\n".join(player_string) + '\n'
     assume(tomlkit.loads(expected_toml))
     assert header_dump == expected_toml
 
